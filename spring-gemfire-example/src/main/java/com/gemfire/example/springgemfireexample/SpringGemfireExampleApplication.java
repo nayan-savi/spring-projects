f698dba5d6bf9cb54046bbb6a6ade3dfc43264aa
Nayan Kumar H N <nayan.savi@gmail.com>
2019-01-19 06:35:10 -0800

    gemfire example

---
+++ spring-gemfire-example/src/main/java/com/gemfire/example/springgemfireexample/SpringGemfireExampleApplication.java
@@ -0,0 +1,14 @@
+package com.gemfire.example.springgemfireexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGemfireExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGemfireExampleApplication.class, args);
+	}
+
+}
+
