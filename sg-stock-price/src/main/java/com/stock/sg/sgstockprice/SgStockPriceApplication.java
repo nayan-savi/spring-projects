972e5031ad95d556683feb5234f6103b1cc20331
Nayan Kumar H N <nayan.savi@gmail.com>
2019-01-29 10:40:45 -0800

    stock project hackerearth

---
+++ sg-stock-price/src/main/java/com/stock/sg/sgstockprice/SgStockPriceApplication.java
@@ -0,0 +1,14 @@
+package com.stock.sg.sgstockprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgStockPriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgStockPriceApplication.class, args);
+	}
+
+}
+
---
+++ sg-stock-price/src/test/java/com/stock/sg/sgstockprice/SgStockPriceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.stock.sg.sgstockprice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SgStockPriceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
